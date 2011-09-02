03e8ae0f1d8467f5008cd2f3e6cc8c57266e2c0d
Vicent Marti <tanoku@gmail.com>
2011-09-02 04:53:14 +0200

    stack: Properly zero-out the stack after reallocations

--- src/stack.c
+++ src/stack.c
@@ -4,7 +4,7 @@
 int
 stack_grow(struct stack *st, size_t new_size)
 {
-	void *new_st;
+	void **new_st;
 
 	if (st->asize >= new_size)
 		return 0;
@@ -12,6 +12,9 @@ stack_grow(struct stack *st, size_t new_size)
 	new_st = realloc(st->item, new_size * sizeof(void *));
 	if (new_st == NULL)
 		return -1;
+
+	memset(new_st + st->asize, 0x0,
+		(new_size - st->asize) * sizeof(void *));
 
 	st->item = new_st;
 	st->asize = new_size;
