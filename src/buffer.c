9b19f58433964e6ab04da4e84ec2e78e4e6c93c9
Roddi <roddi@me.com>
2011-09-21 18:48:13 +0200

    really fixed the crashbug

--- src/buffer.c
+++ src/buffer.c
@@ -136,7 +136,7 @@ bufput(struct buf *buf, const void *data, size_t len)
 void
 bufputs(struct buf *buf, const char *str)
 {
-    if (str)
+    if (!str)
         return;
         
 	bufput(buf, str, strlen(str));
