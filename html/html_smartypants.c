844ce0e21936a9264f8fbedb1a946c738dc4c97a
Chad Weider <cweider@oofn.net>
2011-11-25 13:43:16 -0800

    Do not replace contents of <style> blocks.

--- html/html_smartypants.c
+++ html/html_smartypants.c
@@ -264,8 +264,8 @@ smartypants_cb__dquote(struct buf *ob, struct smartypants_data *smrt, uint8_t pr
 static size_t
 smartypants_cb__ltag(struct buf *ob, struct smartypants_data *smrt, uint8_t previous_char, const uint8_t *text, size_t size)
 {
-	static const char *skip_tags[] = {"pre", "code", "kbd", "script"};
-	static const size_t skip_tags_count = 4;
+	static const char *skip_tags[] = {"pre", "code", "kbd", "script", "style"};
+	static const size_t skip_tags_count = 5;
 
 	size_t tag, i = 0;
 
