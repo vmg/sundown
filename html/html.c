8e5b0d14c9021981dbad364c6860c2ab18a40666
Vicent Martí <tanoku@gmail.com>
2012-03-30 00:05:24 +0200

    Fix setting the level offset when parsing multiple docs

--- html/html.c
+++ html/html.c
@@ -491,7 +491,9 @@ toc_header(struct buf *ob, const struct buf *text, int level, void *opaque)
 {
 	struct html_renderopt *options = opaque;
 
-	if (options->toc_data.header_count == 0) {
+	/* set the level offset if this is the first header
+	 * we're parsing for the document */
+	if (options->toc_data.current_level == 0) {
 		options->toc_data.level_offset = level - 1;
 	}
 	level -= options->toc_data.level_offset;
