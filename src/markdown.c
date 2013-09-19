89c73d27bfab6394b5ec87f10ca75f2c0bddf0f3
Ryan McGeary <ryan@mcgeary.org>
2013-09-19 11:08:17 -0500

    Added support for org-table syntax
    This just adds support for using a plus (+) as an intersection character
    instead of requiring pipes (|). The emacs org-mode table syntax
    automatically manages ascii tables, but uses pluses for line
    intersections.

--- src/markdown.c
+++ src/markdown.c
@@ -2264,7 +2264,7 @@ parse_table_header(
 		while (i < under_end && data[i] == ' ')
 			i++;
 
-		if (i < under_end && data[i] != '|')
+		if (i < under_end && data[i] != '|' && data[i] != '+')
 			break;
 
 		if (dashes < 3)
