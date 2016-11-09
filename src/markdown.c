6e07c0cae47aaa671c8878a7565ddd98f36804b6
Jiri Kratochvil <jiri@apiary.io>
2016-11-09 11:02:54 +0100

    Fix behavior for lists
    there were diferent result (in sourcemap)
    ```
    + a
      b
    + c
    ```
    
    and
    
    ```
    + a
      b
    
    + c
    ```
    
    First one is parsed as "span" - both lines in one block
    Second one was parsed with as "two line block"
    
    This fix make same behavior for sourcemaps for both cases

--- src/markdown.c
+++ src/markdown.c
@@ -1786,7 +1786,7 @@ parse_listitem(struct buf *ob, struct sd_markdown *rndr, uint8_t *data, size_t s
 	}
 
 	/* render of li contents */
-	if (has_inside_empty)
+	//if (has_inside_empty)
 		*flags |= MKD_LI_BLOCK;
 
 	if (*flags & MKD_LI_BLOCK) {
